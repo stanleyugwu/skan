132cbf866c4d944904ed6984b089d45142052942
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-01-27 15:51:12 +0100

    improvement: modify `Text` component to accept `type` prop and modify app constant Colors

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -7,6 +7,7 @@ import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+import {typeStyle} from '../lib/appStyles';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
@@ -22,19 +23,22 @@ export function useThemeColor(
   }
 }
 
+/** Type of text to render whether titled text, body text, etc */
+type textType = keyof typeof typeStyle
+
 type ThemeProps = {
   lightColor?: string;
   darkColor?: string;
 };
 
-export type TextProps = ThemeProps & DefaultText['props'];
+export type TextProps = ThemeProps & DefaultText['props'] & {type?:textType};
 export type ViewProps = ThemeProps & DefaultView['props'];
 
 export function Text(props: TextProps) {
-  const { style, lightColor, darkColor, ...otherProps } = props;
+  const { style, lightColor, darkColor, type = "body", ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+  return <DefaultText style={[{ color },typeStyle[type], style]} {...otherProps} />;
 }
 
 export function View(props: ViewProps) {
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,14 +5,14 @@ const tintColorDark = tw.color('secondary');
 
 export default {
   light: {
-    text: tw.color('on-secondary'),
+    text: tw.color('on-background'),
     background: tw.color('background'),
     tint: tintColorLight,
     tabIconDefault: '#999',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ffffff',
+    text: '#eee',
     background: tw.color('on-background'),
     tint: '#00C4B4',
     tabIconDefault: '#ccc',
