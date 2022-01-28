1a5e180d5e5f966c91d632dd596cf634c29cd569
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-01-28 23:50:38 +0100

    feat: register `home` and `settings` screens to navigator and remove stack navigator

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,21 +1,17 @@
-/**
- * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
 import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Theme } from '@react-navigation/native';
 import { NavigationContainer, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+import HomeScreen from '../screens/HomeScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import { RootState, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
@@ -36,7 +32,7 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
     <NavigationContainer
       linking={LinkingConfiguration}
       theme={colorScheme === 'dark' ? DarkTheme : defaultTheme}>
-      <RootNavigator />
+      <BottomTabNavigator/>
     </NavigationContainer>
   );
 }
@@ -45,19 +41,22 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
  * A root stack navigator is often used for displaying modals on top of all other content.
  * https://reactnavigation.org/docs/modal
  */
-const Stack = createNativeStackNavigator<RootStackParamList>();
+// const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function RootNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      {/* <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group> */}
-    </Stack.Navigator>
-  );
-}
+// function RootNavigator() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
+//       {/* <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+//       <Stack.Group screenOptions={{ presentation: 'modal' }}>
+//         <Stack.Screen name="Modal" component={ModalScreen} />
+//       </Stack.Group> */}
+//     </Stack.Navigator>
+//   );
+// }
+
+
+
 
 /**
  * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
@@ -67,28 +66,29 @@ const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
   const colorScheme = useColorScheme();
+  const favoriteNetwork = useSelector((state:RootState) => state.settings.favoriteNetwork);
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName={favoriteNetwork ? "Settings" : "Top-Up"}
       screenOptions={{
         tabBarStyle: {backgroundColor:Colors[colorScheme].background},
         tabBarActiveTintColor: Colors[colorScheme].tint,
         tabBarInactiveTintColor:Colors[colorScheme].tabIconDefault
       }}>
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+        name="Top-Up"
+        component={HomeScreen}
+        options={({ navigation }: RootTabScreenProps<'Top-Up'>) => ({
+          title: 'Top-Up',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="Settings"
+        component={SettingsScreen}
         options={{
-          title: 'Tab Two',
+          title: 'Settings',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
